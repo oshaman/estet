aed6a4a8a9c7a4f4b6ffb4904a9f8876e7f718a1
oshaman <reg_forall@bigmir.net>
2017-07-04 17:12:04 +0300

    install

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Fresh\Estet\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MainController extends Controller
+{
+    public function index()
+    {
+        dd('test');
+    }
+}
