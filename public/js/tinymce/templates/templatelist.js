285d2e21a86d875ea477cffe5a1a25bad1697726
oshaman <ref_forall@bigmir.net>
2017-08-30 10:12:58 +0300

    public add

---
+++ public/js/tinymce/templates/templatelist.js
@@ -0,0 +1,5 @@
+var tinyMCETemplateList = [
+    // Name, URL, Description
+    ["Две картинки", "/js/tinymce/templates/two_imgs.html", "Две картинки"],
+    ["Картинка слева", "/js/tinymce/templates/image_left.html", "Текст с описанием"],
+];
